8e104965718ff6c9eaa6139a616c472eaa9350a6
ownsterDev <56941965+ownsterDev@users.noreply.github.com>
2020-04-17 23:00:56 -0400

    fixed redirect error

--- welcome.js
+++ welcome.js
@@ -1,6 +1,6 @@
 window.onload = pageR()
 function pageR() {
   console.log("redirecting...")
-  setTimeout(function() {window.location = "../index.html"}, 4000)
+  setTimeout(function() {window.location = "../main.html"}, 4000)
   console.log("redirected")
 }
