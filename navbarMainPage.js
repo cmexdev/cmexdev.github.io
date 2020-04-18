84f5a3f34d7fee08945f24cd1815bc0d4c5eb5aa
ownsterDev <56941965+ownsterDev@users.noreply.github.com>
2020-04-17 22:57:19 -0400

    changed the index.html file

---
+++ navbarMainPage.js
@@ -0,0 +1,6 @@
+var navbarContent;
+var el = document.getElementById('navbarsys')
+
+navbarContent = '<h1 class="navbar"><a href="index.html">piccolowen</a> - <a href="code.html">code</a> - <a href="music.html">music</a></h1>'
+
+el.innerHTML = navbarContent
