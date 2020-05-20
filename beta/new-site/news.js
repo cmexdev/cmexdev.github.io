b952644ff27e44eeba018cf571eb5c7511d13740
ownsterDev <56941965+ownsterDev@users.noreply.github.com>
2020-05-20 19:02:16 -0400

    improved some stuff

--- beta/new-site/news.js
+++ beta/new-site/news.js
@@ -1,7 +1,7 @@
 var out = document.getElementById('news-in')
 
 var newsTitleTxt = 'News panel!'
-var newsDescTxt = 'piccolowen.github.io is going to support a form of API! (if we can figure it out)'
+var newsDescTxt = '<code>piccolowen.github.io</code> suports API! Visit the <a style="font-size:20px;" href="https://github.com/Piccolowen/piccolowen.github.io/wiki/API">wiki</a> for more info.'
 
 var newsCss = document.createElement('link')
 newsCss.href = 'news.css'
@@ -15,8 +15,11 @@ out.appendChild(newsTitle)
 
 var newsDesc = document.createElement('div')
 newsDesc.className = 'news-desc'
-newsDesc.textContent = newsDescTxt
+newsDesc.innerHTML = newsDescTxt
 out.appendChild(newsDesc)
+
+var newsOut = document.createElement('div')
+newsOut.className = 'news-article'
 
 var items = []
 
@@ -27,17 +30,17 @@ $.getJSON("news.json", function (data) {
         var newsTitle = document.createElement('div')
         newsTitle.textContent = data[dataarr].title
         newsTitle.className = 'news-title'
-        out.appendChild(newsTitle)
+        newsOut.appendChild(newsTitle)
 
         var newsAuthor = document.createElement('div')
         newsAuthor.textContent = data[dataarr].author
         newsAuthor.className = 'news-author'
-        out.appendChild(newsAuthor)
+        newsOut.appendChild(newsAuthor)
 
         var newsContent = document.createElement('div')
         newsContent.textContent = data[dataarr].content
         newsContent.className = 'news-content'
-        out.appendChild(newsContent)
+        newsOut.appendChild(newsContent)
 
         var newsWritten = document.createElement('div')
         if (data[dataarr].date == undefined) {
@@ -47,10 +50,11 @@ $.getJSON("news.json", function (data) {
             newsWritten.textContent = 'Written on: ' + data[dataarr].date
         }
         newsWritten.className = 'news-date'
-        out.appendChild(newsWritten)
+        newsOut.appendChild(newsWritten)
 
         dataarr++
     }
+    var mainOut = document.getElementById('news-in').appendChild(newsOut)
 });
 
 var newsHr = document.createElement('hr')
